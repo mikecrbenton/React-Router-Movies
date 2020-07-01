891b9f1b8fa0fc0fa7a4d251e3b2cd39d0908c38
mikecrbenton <mikecrbenton@hotmail.com>
2020-06-30 19:22:36 -0500

    mvp finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
+
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
+  //GET MOVIES -> LOCAL SERVER 5000==============
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -20,15 +24,23 @@ const App = () => {
     }
     getMovies();
   }, []);
+  //============================================
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
+
+
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Route exact path="/">
+         <MovieList movie={movieList} />
+      </Route>
+
+      <Route exact path="/movies/:id" component={Movie}/>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,35 +1,59 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+//import App from '../App';
 
 const MovieList = props => {
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
+
+      { props.movie ? 
+      <div>
+         { props.movie.map( eachMovie => (
+               <MovieDetails key={ eachMovie.id } movie={ eachMovie } />
+         ) ) }
+      </div> :
+      <p>LOADING</p> }
+
+
     </div>
   );
 }
 
+// ***THIS IS A NEW CONCEPT*** - A COMPONENT CAN BE DEFINED WITHIN ANOTHER
+// IT DOESN'T HAVE TO BE IN A SEPARATE FILE
+
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+  return (
+   
+   <Link to={`/movies/${movie.id}`}>
+      <div className="movie-card">
+
+            <h2>{title}</h2>
+
+            <div className="movie-director">
+            Director: <em>{director}</em>
+            </div>
+
+            <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+            </div>
+
+            <h3>Actors</h3>
+
+            {stars.map(star => (
+            <div key={star} className="movie-star">
+               {star}
+            </div>
+            ))}
+
+      </div>
+   </Link>
   );
 }
 
 export default MovieList;
+
+
