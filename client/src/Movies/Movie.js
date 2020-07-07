16e4de97aef0532e248b49328d7c92780705f72c
mikecrbenton <mikecrbenton@hotmail.com>
2020-07-06 19:55:40 -0500

    working on stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard'
 import axios from 'axios';
+import styled from 'styled-components';
 
 function Movie(){
 
@@ -24,41 +26,19 @@ function Movie(){
     },[]);
 
    // Uncomment this only when you have moved on to the stretch goals
-  //  const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+   const saveMovie = () => {
+    //const addToSavedList = props.addToSavedList;
+    //addToSavedList(movie)
+  }
 
     if (!movie) {
       return <div>Loading movie information...</div>;
     }
-  
-    const { title, director, metascore, stars } = movie;
-    return (
-  
-      <div className="save-wrapper">  
-        <div className="movie-card">
-  
-          <h2>{title}</h2>
-  
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-  
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-  
-          <h3>Actors</h3>
-  
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>      
-          ))}
 
-        </div>
-        <div className="save-button">Save</div>
+    return (
+      <div className="save-wrapper">  
+        <MovieCard movie={movie} />
+        <div className="save-button" onClick={saveMovie}>Save</div>
       </div>
     );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import MovieCard from './MovieCard';
 //import App from '../App';
 
 const MovieList = props => {
@@ -24,32 +25,10 @@ const MovieList = props => {
 // IT DOESN'T HAVE TO BE IN A SEPARATE FILE
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
 
-  return (
-   
+  return ( 
    <Link to={`/movies/${movie.id}`}>
-      <div className="movie-card">
-
-            <h2>{title}</h2>
-
-            <div className="movie-director">
-            Director: <em>{director}</em>
-            </div>
-
-            <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-            </div>
-
-            <h3>Actors</h3>
-
-            {stars.map(star => (
-            <div key={star} className="movie-star">
-               {star}
-            </div>
-            ))}
-
-      </div>
+      <MovieCard movie={movie} />        
    </Link>
   );
 }
